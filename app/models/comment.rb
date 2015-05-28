8cc45167a4d0872a4f3016cdcd89c7e2d3425274
Austin Archibald <austin.archibald@gmail.com>
2015-05-28 14:10:04 -0600

    added drop down of stars for rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+
+  @@star = '&#9733'.html_safe
+
+  RATINGS = {
+    @@star                                      => '1_star',
+    @@star + @@star                             => '2_stars',
+    @@star + @@star + @@star                    => '3_stars',
+    @@star + @@star + @@star + @@star           => '4_stars',
+    @@star + @@star + @@star + @@star + @@star  => '5_stars'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
