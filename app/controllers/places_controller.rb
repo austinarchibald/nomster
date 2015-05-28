61aea100adf1b0c568add80479f988f9fead346c
Austin Archibald <austin.archibald@gmail.com>
2015-05-28 11:48:40 -0600

    added validation to place name

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,7 +11,11 @@ class PlacesController < ApplicationController
 
   def create
     current_user.places.create(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, :status => :unprocessable_entity
+    end
   end
 
   def show
@@ -31,7 +35,11 @@ class PlacesController < ApplicationController
       return render :text => 'Nice try, Marco', :status => :forbidden
     end
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, :status => :unprocessable_entity
+    end
   end
 
   def destroy
